1ea0a4607d67f99ad9d395654495f9ce367d824f
Tin Tran <gg.tintran@gmail.com>
2024-04-12 22:26:11 +0700

    update

---
+++ Minibox.Presentation.Share.Module/Logging/LoggingExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minibox.Presentation.Share.Module.Logging
+{
+    internal class LoggingExtension
+    {
+    }
+}
